82baf2380db638979f4e32587872e45f37549ee3
R.M. Kleywegt <rmk@getpique.com>
2021-05-16 20:51:00 +0200

    fix(config): fix domain handling for analytics

--- lib/config.ts
+++ lib/config.ts
@@ -115,7 +115,7 @@ export const fathomId = isDev ? null : process.env.NEXT_PUBLIC_FATHOM_ID
 
 export const fathomConfig = fathomId
   ? {
-      excludedDomains: ['localhost', 'localhost:3000']
+      excludedDomains: ['localhost', 'localhost:3000', isDev ? domain : undefined]
     }
   : undefined
 
