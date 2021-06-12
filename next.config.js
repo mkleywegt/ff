3ca9f0c34f1f86dfcb8446e60636f1afda0fdd79
R.M. Kleywegt <rmk@getpique.com>
2021-06-12 12:25:25 +0200

    fix(config): fix image handling for static export

--- next.config.js
+++ next.config.js
@@ -1,20 +1,29 @@
 // const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+const bundleAnalyzer = require('@next/bundle-analyzer')
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
-  images: {
-    domains: ['pbs.twimg.com']
-  },
-  future: {
-    webpack5: true
-  },
-  env: {
-    'ROOT_NOTION_PAGE_ID': process.env.ROOT_NOTION_PAGE_ID,
-    'SITE_NAME': process.env.SITE_NAME,
-    'SITE_URL': process.env.SITE_URL,
-    'SITE_AUTHOR': process.env.SITE_AUTHOR,
+module.exports = withPlugins(
+  [
+    [bundleAnalyzer, {
+      enabled: process.env.ANALYZE === 'true'
+    }],
+    [optimizedImages, {
+      handleImages: ['jpeg', 'png', 'svg', 'webp']
+    }],
+  ],{
+    images: {
+      loader: "imgix",
+      path: "https://noop/",
+    },
+    future: {
+      webpack5: true
+    },
+    env: {
+      'ROOT_NOTION_PAGE_ID': process.env.ROOT_NOTION_PAGE_ID,
+      'SITE_NAME': process.env.SITE_NAME,
+      'SITE_URL': process.env.SITE_URL,
+      'SITE_AUTHOR': process.env.SITE_AUTHOR,
+    }
   }
-});
+);
